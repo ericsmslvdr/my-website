c97dccb78058bf037f7151b044917b8975f44929
Eric Samuel Salvador <ericsmsalvador@gmail.com>
2023-11-27 14:48:26 +0800

    Add navbar effect

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const isScrolled = window.scrollY > 0;
+            setScrolled(isScrolled);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className="navbar">
+        <nav className={scrolled ? 'navbar scrolled' : 'navbar'}>
             <div className="logo"><a href="#home">&lt;&#47;&gt; Eric Samuel Salvador</a></div>
             <div className="menu-bar">
                 <ul>
